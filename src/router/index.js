2d9dde866f37c0309d48e20ed023dc227f1c0bcd
illuscript <theivankhor@gmail.com>
2026-02-21 19:18:51 +0800

    minor fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NormalTimerView from '../views/NormalTimerView.vue'
-import PomodoroTimerView from '../views/PomodoroTimerView.vue'
 import AnalyticsView from '../views/AnalyticsView.vue'
+import PomodoroTimerView from '../views/PomodoroTimerView.vue'
 
 const routes = [
   { path: '/',           name: 'Home',     component: HomeView },
@@ -12,6 +12,6 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
